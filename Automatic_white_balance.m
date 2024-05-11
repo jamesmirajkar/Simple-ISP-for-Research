a6e26a312af55593caded29216e7c7365d0e6358
yashas manjunath hittalmakki <89625729+mig9mili@users.noreply.github.com>
2024-05-11 20:33:27 +0530

    Update Automatic_white_balance.m

--- Automatic_white_balance.m
+++ Automatic_white_balance.m
@@ -1,14 +1,14 @@
 function balancedImage = Automatic_white_balance(demosaicedImage, bitsPerSample)
-    avgR = mean2(demosaicedImage(:, :, 1));
-    avgG = mean2(demosaicedImage(:, :, 2));
-    avgB = mean2(demosaicedImage(:, :, 3));
+    avgRed = mean2(demosaicedImage(:, :, 1));
+    avgGreen = mean2(demosaicedImage(:, :, 2));
+    avgBlue = mean2(demosaicedImage(:, :, 3));
 
-    gainR = avgG/ avgR;
-    gainB = avgG/ avgB;
+    gainR = avgGreen/ avgRed;
+    gainB = avgGreen/ avgBlue;
 
     balancedImage = demosaicedImage;
-    balancedImage(:, :, 1) = (balancedImage(:, :, 1) * gainR);
-    balancedImage(:, :, 3) = (balancedImage(:, :, 3) * gainB);
+    balancedImage(:, :, 1) = (balancedImage(:, :, 1) * gainRed);
+    balancedImage(:, :, 3) = (balancedImage(:, :, 3) * gainBlue);
 
    
    %balancedImage = uint16(min(max(balancedImage, 0), (2^bitsPerSample - 1)));

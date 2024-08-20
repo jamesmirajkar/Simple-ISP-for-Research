2c284cd95236cbaf644746aba10bc0f4718e48b7
yashas manjunath hittalmakki <89625729+mig9mili@users.noreply.github.com>
2024-08-20 11:16:31 +0530

    Update tone_mapping.m

--- tone_mapping.m
+++ tone_mapping.m
@@ -1,24 +1,24 @@
 function result_image = tone_mapping(input_image)
-%% Convert input image to double
-input_image = double(input_image);
+% Convert input image to double and reshape for vectorized operations
+input_image = reshape(double(input_image), [], 3);
 
-%% Parameters
+% Parameters
 delta = 1e-6;
 a = 0.0079;
 L_white = max(input_image(:));
 
-%% Logarithmic mean luminance
-luminance = 0.2126 * input_image(:,:,1) + 0.7152 * input_image(:,:,2) + 0.0722 * input_image(:,:,3);
-log_luminance = log(delta + luminance);
-Lw = sum(log_luminance(:)) / numel(input_image);
-Lw1 = exp(Lw);
+% Vectorized luminance calculation
+luminance = input_image * [0.2126; 0.7152; 0.0722];
 
-%% Scaled luminance
+% Logarithmic mean luminance (vectorized)
+Lw1 = exp(mean(log(delta + luminance)));
+
+% Scaled luminance (vectorized)
 L = (a / Lw1) * input_image;
 
-%% Tone mapping operator
+% Tone mapping operator (vectorized)
 Ld = L .* (1 + L / (L_white^2)) ./ (1 + L);
 
-%% Output
-result_image = Ld;
+% Reshape output to original dimensions
+result_image = reshape(Ld, size(input_image, 1), size(input_image, 2), 3);
 end
